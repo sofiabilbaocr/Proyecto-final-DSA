459910ac17a361b66107de226ca513f720e4b2f7
Paula Sofia Bilbao Cruz <sofia.bilbao@ufm.edu>
2026-05-14 14:22:52 -0600

    Play_next added

--- player.py
+++ player.py
@@ -8,7 +8,7 @@ from double_stack import DoubleStack
  
 class MusicPlayer:
     def __init__(self):
-        self.stack = DoubleStack(20)
+        self.stack = DoubleStack(20)  #tamaño del stack o sea maximo de canciones del reproductor
         self.current_song = None        #es la canción que está sonando
  
     def __repr__(self) -> str:
@@ -20,3 +20,13 @@ class MusicPlayer:
     def add_song(self, song: str) -> None:
         #arega una canción al stack de siguientes
         self.stack.push_next(song)
+
+
+    def play_next(self) -> str:
+        #verifica que haya una canción siguiente
+        if self.stack.top_next == self.stack.size:
+            return 'No hay canciones siguientes.'
+        
+    #l canción actual pasa al stack de anteriores
+        if self.current_song is not None:
+            self.stack.push_prev(self.current_song)
